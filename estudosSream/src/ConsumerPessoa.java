95e17856b9c02135caaf1eb45c7ee710475a7598
leandro Santana <leandro.edv@hotmail.com>
2024-09-23 17:31:23 -0300

    estudos com Sream API

---
+++ estudosSream/src/ConsumerPessoa.java
@@ -0,0 +1,35 @@
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConsumerPessoa {
+    private List<Pessoa> pessoaList;
+
+    public ConsumerPessoa() {
+        this.pessoaList = new ArrayList<>();
+    }
+    public void  adicionarPessoa(String nome, int idade){
+        pessoaList.add(new Pessoa(nome,idade));
+    }
+    public void listarPessoas(){
+        System.out.println(pessoaList);
+    }
+    public void pessoasPorIdade(){
+        List<Pessoa> pessoasIdade = new ArrayList<>(pessoaList);
+        pessoasIdade.stream()
+                .sorted((p1, p2) -> Integer.compare(p1.getIdade(),p2.getIdade()))
+                .forEach(pessoa -> System.out.println(pessoa.getNome() + " tem: " + pessoa.getIdade()));
+
+    }
+    public static void main(String[] args) {
+        ConsumerPessoa consumerPessoa = new ConsumerPessoa();
+        consumerPessoa.adicionarPessoa(" joão",25);
+        consumerPessoa.adicionarPessoa(" pedro",35);
+        consumerPessoa.adicionarPessoa(" maria",15);
+        consumerPessoa.adicionarPessoa(" Deise",31);
+        consumerPessoa.pessoasPorIdade();
+        consumerPessoa.listarPessoas();
+
+    }
+
+}
